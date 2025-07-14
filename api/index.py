4f29e57bc2c01bd4e6c7b41b45ef7376b3f93d77
nova-putri-herni <pkm36@teknokrat.ac.id>
2025-07-14 16:25:23 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -129,7 +129,7 @@ class ArtikelAPI:
 
 
 config_artikel = ArtikelAPI()
-config_artikel.sheet_id('14EXDQFZ3w3m20W1w3EBl0MGCO9hxNz63ZWhC4tQewYY')
+config_artikel.sheet_id('1EV-Cdy47mQuPHLf6dqRKZiR100BJLYlRJn7JW1UocKE')
 config_artikel.gid('710523043')
 
 
